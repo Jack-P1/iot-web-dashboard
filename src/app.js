53c1d6d7307da090d36b9ef3a4ee71b3b0563a77
Jack-P1 <jackpilbeam101@gmail.com>
2025-03-04 17:34:19 +0000

    setup FE code repo

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const routes = require('./routes/routes')
+
+app.use('/api', routes)
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
---
+++ src/routes/routes.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+
+/*
+define routes here
+
+router.get()
+
+*/
+
+
+module.exports = router
