949fdbf4143c0ac1afd1fec1066c0350fd8693b2
Jack-P1 <jackpilbeam101@gmail.com>
2025-03-25 21:53:46 +0000

    write to database on response in cron

--- src/cron/jobs.js
+++ src/cron/jobs.js
@@ -2,28 +2,36 @@ const MqttTopic = require('../database/mqttTopicModel')
 
 /*
     Get MQTT feed data at a set interval defined in app.js.
-    This could be improved by implementing concurrency and limiting the amount of API requests (e.g. p-limit).
-
-    TODO: write to database
+    This could be improved by implementing concurrency, limiting the amount of API requests (e.g. p-limit), and looking at a distributed architecture (microservice, aws lambda).
+    TODO: test database write with multiple response objects
 */
 exports.getBatchMqttData = async () => {
-    const results = await MqttTopic.getAllTopicAndBranchId()
+    try{
+        const results = await MqttTopic.getAllTopicAndBranchId()
 
-    const endpoints = results.map(result => fetch(`https://io.adafruit.com/api/v2/${process.env.MQTT_USERNAME}/groups/${result.groupKey}/feeds/${result.topic}/data?limit=1`,
-        {
-            method: "GET",
-            headers: {
-                "Content-Type": 'application/json',
-                "x-aio-key": process.env.ADAFRUIT_AIO_KEY,
-            },
-        }).then(res => res.json().then(json => ({
-            data: json.length > 0 ? json[0].value : null,
-            id: result.id,
-            topic: result.topic
-        })))
-    )
+        const endpoints = results.map(result => fetch(`https://io.adafruit.com/api/v2/${process.env.MQTT_USERNAME}/groups/${result.groupKey}/feeds/${result.topic}/data?limit=1`,
+            {
+                method: "GET",
+                headers: {
+                    "Content-Type": 'application/json',
+                    "x-aio-key": process.env.ADAFRUIT_AIO_KEY,
+                },
+            }).then(res => res.json().then(json => ({
+                data: json.length > 0 ? json[0].value : null,
+                id: result.id,
+                topic: result.topic
+            })))
+        )
+    
+        const responses = await Promise.all(endpoints);
+    
+        if(responses){
+            await MqttTopic.bulkUpdateById(responses)
+        }
 
-    const responses = await Promise.all(endpoints);
+    } catch(err) {
+        // TODO add proper logging
+        console.log(err)
+    }
 
-    console.log(responses)
 }
--- src/database/mqttTopicModel.js
+++ src/database/mqttTopicModel.js
@@ -8,6 +8,26 @@ const db = new sqlite.Database(dbPath, sqlite.OPEN_READWRITE, (err) => {
     }
 });
 
+exports.bulkUpdateById = async (params) => {
+    return new Promise((resolve, reject) => {
+        if(params?.length){
+            db.serialize(() => {
+                db.run("begin transaction");
+    
+                for (let i = 0; i < params.length; i++){
+                    db.run("UPDATE mqtt_topic SET last_payload = (?) WHERE mqtt_topic.id = (?)", params[i].data, params[i].id, (err) => {
+                        if (err) reject(err);
+                    })
+                }
+
+                db.run("commit");
+            })
+        } else {
+            reject()
+        }
+    })
+}
+
 exports.createNewTopic = async (params) => {
     return new Promise((resolve, reject) => {
         db.run('INSERT INTO mqtt_topic (topic, qos, last_payload, itemId) VALUES (?,?,?,?)', [params.topic, params.qos, params.last_payload, params.itemId], (err) => {
