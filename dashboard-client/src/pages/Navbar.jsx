ede70d2ef081f168c433cd2c0145c3425f60927d
Jack-P1 <jackpilbeam101@gmail.com>
2025-04-11 11:44:12 +0100

    navbar collapse button

--- dashboard-client/src/pages/Navbar.jsx
+++ dashboard-client/src/pages/Navbar.jsx
@@ -19,22 +19,16 @@ function Navbar(){
     if(!token || !user) return null;
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-success">
+        <nav className="navbar navbar-expand-md navbar-light bg-success">
             <a className="navbar-brand" href="#">{user.name} <br /> {user.role}</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
 
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <div className="collapse navbar-collapse" id="navbarContent">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item">
                         <Link className="nav-link" to={`/home`}>Home</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to={`/home`}>Branches</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to={`/home`}>Profile</Link>
                     </li>
                     <li className="nav-item">
                         <button className="nav-link" onClick={handleLogout}> Logout </button>
